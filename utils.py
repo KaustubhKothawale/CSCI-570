964d7b3c444a09f8d996ccdc67f2d887802842a6
surbhikhatri <surbhikhatri@Surbhis-MacBook-Pro.local>
2022-12-07 19:15:04 -0800

    add string generation script

---
+++ utils.py
@@ -0,0 +1,55 @@
+import sys
+
+
+# Read from the target input file.
+def read_input():
+    # get file name from command arguments.
+    file = sys.argv[1]
+    f = open(file, "r")
+
+    base_string_list = []
+    index_list1 = []
+    index_list2 = []
+    is_first_done = False
+    for line in f.readlines():
+        line = line.strip()
+        if not line.isnumeric():
+            base_string_list.append(line)
+            if len(base_string_list) == 2:
+                is_first_done = True
+        else:
+            if not is_first_done:
+                index_list1.append(int(line))
+            else:
+                index_list2.append(int(line))
+    f.close()
+    return base_string_list, index_list1, index_list2
+
+
+# return 2 generated lists.
+def string_generator_wrapper():
+    base_string_list, index_list1, index_list2 = read_input()
+    result = [string_generator(index_list1, base_string_list[0]), string_generator(index_list2, base_string_list[1])]
+    # print(result)
+    return result
+
+
+# logic to generate new strings.
+def string_generator(index_list, base_string):
+    index = 0
+    while index < len(index_list):
+        base_string = base_string[0:index_list[index] + 1] + base_string + base_string[index_list[index] + 1:]
+        index += 1
+    return base_string
+
+
+# function to write new string to strings.txt file.
+def write_output():
+    result = string_generator_wrapper()
+    f = open("strings.txt", 'wt')
+    f.write('\n'.join(result))
+    f.close()
+    return result
+
+
+write_output()
